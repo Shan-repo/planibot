8457792166876291e37dc8417833a1e869a0b4a1
Shanavas <57465591+Shanavas-Repo@users.noreply.github.com>
2020-10-29 17:47:28 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import './App.css';
+// import {Layout, Header, Navigation, Drawer, Content,Footer,FooterSection,FooterLinkList} from 'react-mdl';
+// import Main from './components/main';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import 'react-mdl/extra/material.css';
 import  'react-mdl/extra/material.js';
+
 
 function App() {
 
  
 
   return (
-    
-    <div className="demo-big-content">
+    // className="demo-big-content"
+    <div >
     {/* <Layout >
         <Header>
         <div style={{ float: 'left' }}><img src="https://cdn.planittesting.com/planit/media/siteimages/components/planit-logo-140.png" width="50" alt="logo" /></div>
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -43,6 +43,59 @@ import { useHistory } from "react-router-dom";
       height: 140,
     },
   }));
+
+  function webchat () {
+     const styleOptions = {
+               // Add styleOptions to customize web chat canvas
+               hideUploadButton: true
+            };
+
+            // Add your BOT ID below
+            var BOT_ID = "41606844-637c-4f67-bb0b-879bbbafe63d"; 
+
+            var theURL = "https://powerva.microsoft.com/api/botmanagement/v1/directline/directlinetoken?botId=" + BOT_ID;
+
+            const store = window.WebChat.createStore(
+               {},
+               ({ dispatch }) => next => action => {
+                   if (action.type === "DIRECT_LINE/CONNECT_FULFILLED") {
+                      dispatch({
+                          meta: {
+                               method: "keyboard",
+                           },
+                           payload: {
+                               activity: {
+                                     channelData: {
+                                          postBack: true,
+                                     },
+                                      //Web Chat will show the 'Greeting' System Topic message which has a trigger-phrase 'hello'
+                                      name: 'startConversation',
+                                      type: "event"
+                                 },
+                            },
+                            type: "DIRECT_LINE/POST_ACTIVITY",
+                       });
+                 }
+                 return next(action);
+              }
+           );
+           fetch(theURL)
+                .then(response => response.json())
+                .then(conversationInfo => {
+                    window.WebChat.renderWebChat(
+                        {
+                            directLine: window.WebChat.createDirectLine({
+                                token: conversationInfo.token,
+                            }),
+                            store: store,
+                            styleOptions: styleOptions
+                        },
+                        document.getElementById('webchat')
+                    );
+                })
+                .catch(err => console.error("An error occurred: " + err));
+
+  }
   
 
 
@@ -53,11 +106,10 @@ export default function Homepage() {
     return ( 
       
       <div className={classes.containers}>
-      {/* <iframe 
+      <iframe 
             style={{float:'right',maxWidth:500,width:'100%', height:'100%', overflow:'visible'}}
             src="https://powerva.microsoft.com/webchat/bots/41606844-637c-4f67-bb0b-879bbbafe63d"    
-        /> */}
-
+        />
   </div>
 
     );  
